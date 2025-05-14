98fa6d5e5a8fb2d6055c7a11113175a0e3c88c44
Mr Spike <87905686+ayan-lab@users.noreply.github.com>
2025-05-14 17:58:46 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "https://chattybackend.vercel.app/api" : "/api",
   withCredentials: true,
-});
+}); 
