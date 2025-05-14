d270d92bf01af972aa85e75e6ca802401edbaf91
Mr Spike <87905686+ayan-lab@users.noreply.github.com>
2025-05-14 18:38:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: "https://astonishing-toffee-10b2e4.netlify.app",
     credentials: true,
   })
 );
