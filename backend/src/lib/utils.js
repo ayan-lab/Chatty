9e7d6a1aa12957d946771539144d1f7ea013bb77
Mr Spike <87905686+ayan-lab@users.noreply.github.com>
2025-05-27 20:23:22 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, //prevents XSS attacks cross-site scripting attacks
-    sameSite: "strict", //CSRF attacks cross-site reques forgery attacks
-    secure: process.env.NODE_ENV !== "developement",
+    sameSite: "none", //CSRF attacks cross-site reques forgery attacks
+    secure: "true",
   });
   return token;
 };
