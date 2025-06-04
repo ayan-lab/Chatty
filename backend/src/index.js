5d061c41c0baae679fb185552339d9e484ccf42a
Mr Spike <87905686+ayan-lab@users.noreply.github.com>
2025-06-04 23:23:24 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://lets-social.vercel.app",
+    origin: "https://chatty-frontend-t18a.onrender.com",
     credentials: true,
   })
 );
